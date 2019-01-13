41c4d4f00f7d4e0d3d5228e12805245e1549ee05
stefangreene <stefangreene@comcast.net>
2019-01-13 00:09:09 -0500

    changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+var compNumber;
+var loses = 0;
+var win = 0;
+var counter = 0;
+
+
+ startGame = function(){
+$(".container-crystal").empty();
+lossBox = $("#loses");
+lossBox.append("Loses: ");
+
+compNumber = Math.floor(Math.random() * 30) + 90;
+console.log("Random number is " + compNumber);
+compContain = $("#comp-number");
+compContain.html("Goal: " + compNumber);
+compContain.addClass("comp-box");
+
+for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() * 10) + 2;
+    console.log(random);
+    var crystal = $("<div>");
+    crystal.attr({"class": 'crystal', "data-random": random});
+    $(".container-crystal").append(crystal);
+    
+}
+    $(function(){
+    var newPhoto = $(".newPhoto").find("<img>").eq(1)
+    newPhoto.attr("src", "../images2/gem1.png");
+    
+    //2 different ways
+    //     var photo = $("<img>")
+    //     photo.attr("src", "images/pic.jpeg");
+    //     $(".newPhoto").append(photo);
+        });
+
+}
+
+startGame();
+
+
+
+$(".container-crystal").on("click", ".crystal" , function () {
+
+   var num = parseInt ($(this).attr("data-random"));
+    counter += num;$("#previous").html("Total Score: " + counter );
+
+    console.log("this is the count " + counter);
+    if (counter > compNumber) {
+        loses++
+        $("#loses").html("Loses: " + loses)
+        alert("You Lost!")
+        counter = 0;
+        startGame()
+    }
+    else if (counter === compNumber) {
+        win++
+        $("#win").html("Wins: " + win)
+        alert("You Won!")
+        counter = 0;
+        startGame()
+    } 
+});
